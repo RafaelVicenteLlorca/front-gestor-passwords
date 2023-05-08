a2981361b7cd17d64f25f1501d48293aa573f839
rvlc1 <rvlc1@alu.ua.es>
2023-05-08 22:06:50 +0200

    añadido mainmenu, singin, singup, clase user usertoString stringtoUser

--- prueba.go
+++ prueba.go
@@ -3,10 +3,10 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/crypto/chacha20"
 	"golang.org/x/crypto/sha3"
@@ -14,21 +14,50 @@ import (
 
 var key []byte
 
-func generadorhas(nombre string, pass string) []byte {
+type User struct {
+	Username string
+	Email    string
+	Password string
+}
+
+func UserToString(usuario User) string {
+	return "Usuario: " + usuario.Username + " email: " + usuario.Email + " contraseña: " + usuario.Password
+}
+
+func StringToUser(linea string) User {
+	// Dividir la cadena en tres partes usando el separador " "
+	partes := strings.Split(linea, " ")
+
+	// Extraer los valores de usuario, email y contraseña
+	username := strings.TrimPrefix(partes[0], "Usuario:")
+	email := strings.TrimPrefix(partes[1], "email:")
+	password := strings.TrimPrefix(partes[2], "contraseña:")
+
+	// Crear una nueva instancia de la estructura User con los valores extraídos
+	user := &User{
+		Username: strings.TrimSpace(username),
+		Email:    strings.TrimSpace(email),
+		Password: strings.TrimSpace(password),
+	}
+
+	return user
+}
+
+func generadorhas(email string, pass string) ([]byte, []byte) {
 	// Concatenamos el usuario y la contraseña separados por un caracter nulo
-	data := []byte(nombre + "\x00" + pass)
+	data := []byte(email + "\x00" + pass)
 
 	// Generamos el hash SHA-3 de 512 bits de la concatenación anterior
 	hash := sha3.Sum512(data)
-
+	hashemaillargo := sha3.Sum256([]byte(email))
 	// Convertimos el hash a una cadena de texto en formato hexadecimal
 	// hashStr := hex.EncodeToString(hash[:])
 
 	// Dividimos el hash en dos partes iguales
 	key = hash[:len(hash)/2]
 	hash2 := hash[len(hash)/2:]
-
-	return hash2
+	emailhashed := hashemaillargo[:]
+	return hash2, emailhashed
 }
 
 func EncryptChaCha(texto string) (string, error) {
@@ -83,31 +112,87 @@ func DecryptChaCha(ciphertext string) (string, error) {
 	return string(plaintext), nil
 }
 
-func main() {
+func mainmenu() {
+	fmt.Println("------------------------------------------------------------------------------------------/n")
+	fmt.Println("-------------------------------BIENVENIDO AL GESTOR DE CONTRASEÑAS------------------------/n")
+	fmt.Println("------------------------------------------------------------------------------------------/n")
+	fmt.Println("/n /n /n")
+	fmt.Println("								Pulse 1 para logearse										/n")
+	fmt.Println("								Pulse 2 para Crear un usuario								/n")
+}
 
-	var nombre string
+func singindata() ([]byte, []byte) {
+	var email string
 	var contraseña string
-	fmt.Println("nombre: ")
-	fmt.Scan(&nombre)
-	fmt.Println("pass: ")
+	fmt.Println("introduzca su email: ")
+	fmt.Scan(&email)
+	fmt.Println("introduzca su contraseña: ")
 	fmt.Scan(&contraseña)
+	login, hashemail := generadorhas(email, contraseña)
+	return login, hashemail
+}
 
-	fmt.Println("Mi nombre es", nombre, "y la contraseña es", contraseña, ".")
+func singin([]byte, []byte) {
+	fmt.Println("to do")
+}
 
-	hashStr := generadorhas(nombre, contraseña)
-	fmt.Println("Hash generado:", hex.EncodeToString(hashStr[:]))
+func singupdata() ([]byte, []byte) {
+	var email string
+	var contraseña string
+	var contraseña2 string
 
-	textocifrado, errorsalida := EncryptChaCha("hola mundo")
-	if errorsalida != nil {
-		fmt.Println(errorsalida)
+	fmt.Println("introduzca su email: ")
+	fmt.Scan(&email)
+	for {
+		fmt.Println("introduzca su contraseña: ")
+		fmt.Scan(&contraseña)
+		fmt.Println("verificando contraseña, introduzca su contraseña de nuevo: ")
+		fmt.Scan(&contraseña2)
+		if contraseña == contraseña2 {
+			break
+		}
 	}
 
-	fmt.Println("texto cifrado:", textocifrado)
+	login, hashemail := generadorhas(email, contraseña)
+	return login, hashemail
+}
 
-	textodescifrado, errorsalida := DecryptChaCha(textocifrado)
-	if errorsalida != nil {
-		fmt.Println(errorsalida)
+func main() {
+	var option string
+	mainmenu()
+	fmt.Scan(&option)
+	switch option {
+	case "1":
+		i := 0
+		for {
+			loginHash, hashemail := singindata()
+			singin(loginHash, hashemail)
+			if i == 3 {
+				fmt.Println("numero de intentos superados, intentelo mas tarde")
+			}
+		}
+
+	case "2":
+		loginHash, hashemail := singupdata()
+		singin(loginHash, hashemail)
+	default:
+		fmt.Println("error al escoger opcion")
 	}
+	/*
+		fmt.Println("Hash generado:", hex.EncodeToString(loginHash[:]))
 
-	fmt.Println("texto descifrado:", textodescifrado)
+		textocifrado, errorsalida := EncryptChaCha("hola mundo")
+		if errorsalida != nil {
+			fmt.Println(errorsalida)
+		}
+
+		fmt.Println("texto cifrado:", textocifrado)
+
+		textodescifrado, errorsalida := DecryptChaCha(textocifrado)
+		if errorsalida != nil {
+			fmt.Println(errorsalida)
+		}
+
+		fmt.Println("texto descifrado:", textodescifrado)
+	*/
 }
